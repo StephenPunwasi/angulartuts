1b02f16c2e0d995fb2e821b5032a652223f906cf
Stephen Punwasi <stephen.punwasi@gmail.com>
2014-11-06 20:17:29 -0500

    Added ng-views and routing

--- single-page-app/script.js
+++ single-page-app/script.js
@@ -1,5 +1,34 @@
-var aboutApp = angular.module('aboutApp', []);
+var aboutApp = angular.module('aboutApp', ['ngRoute']);
 
 aboutApp.controller('mainController', function($scope){
-    $scope.message = 'It Works!'
+    $routeProvider
+
+      //HOMEPAGE
+      .when('/', {
+        templateUrl : 'pages/home.html',
+        controller  : 'mainController'
+      })
+
+      //ABOUT PAGE
+      .when('/about', {
+        templateUrl : 'pages/about.html',
+        controller  : 'aboutController'
+      })
+
+      .when('/contact', {
+          templateUrl : 'pages/contact.html',
+          controller  : 'contactController'
+      });
+});
+
+aboutApp.controller('mainController', function($scope){
+  $scope.message = 'It Works!';
+});
+
+aboutApp.controller('aboutController', function($scope){
+  $scope.message = "I'm the about page!";
+});
+
+aboutApp.controller('contactController', function($scope){
+  $scope.message = "I'm the contact page";
 });
